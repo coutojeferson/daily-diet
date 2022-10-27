652c0afaf52e728f93495a229c59dad8aefa712e
Jeferson <coutojefersonn@gmail.com>
2022-10-27 12:25:15 -0300

    Inicio do projeto.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { DashBoard } from './src/Screens/DashBoard';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <DashBoard />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Screens/DashBoard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { styles } from './styles';
+
+export function DashBoard() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>DashBoard</Text>
+    </View>
+  );
+}
